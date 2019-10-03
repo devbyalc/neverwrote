2be833fc9caa3d052f2af6acd805e1a5176da265
Margareta Alicia Hadisaputra <18858284@students.ltu.edu.au>
2019-10-03 12:42:46 +1000

    finish part 1

--- api/src/controllers/notebooks.js
+++ api/src/controllers/notebooks.js
@@ -4,6 +4,15 @@ const models = require('../models');
 
 const router = express.Router();
 
+
+
+/*models.Note.belongsTo(models.Notebook);
+models.Notebook.hasMany(models.Note);*/
+
+function postFilter(obj) {
+  return _.pick(obj, ['title']);
+}
+
 // Index
 router.get('/', (req, res) => {
   models.Notebook.findAll({ order: [['createdAt', 'DESC']] })
@@ -12,5 +21,38 @@ router.get('/', (req, res) => {
 });
 
 /* *** TODO: Fill in the API endpoints for notebooks *** */
+router.get('/:notebookId/notes',(req,res)=>{
+  models.Note.findAll({where:{notebookId:[req.params.notebookId]}})
+  .then(notes => res.json(notes))
+  .catch(err => res.status(500).json({ error: err.message }));
+});
+
+router.post('/',(req,res) => {
+  models.Notebook.create(postFilter(req.body))
+  .then(notebook => res.json(notebook))
+  .catch(err => res.status(422).json({ error: err.message }));
+});
+
+router.get('/:notebookId',(req,res) => {
+  models.Notebook.findById(req.params.notebookId)
+  .then(notebook => res.json(notebook))
+  .catch(err => res.status(500).json({ error: err.message }));
+});
+
+router.delete('/:notebookId',(req,res) => {
+  models.Notebook.findById(req.params.notebookId)
+    .then(notebook => notebook.destroy())
+    .then(() => res.json({}))
+    .catch(err => res.status(500).json({ error: err.message }));
+});
+
+router.put('/:notebookId',(req,res) => {
+  models.Notebook.findById(req.params.notebookId)
+  .then(notebook => notebook.update(postFilter(req.body)))
+  .then(notebook => res.json(notebook))
+  .catch(err => res.status(422).json({ error: err.message }));
+});
+
+
 
 module.exports = router;
--- api/src/controllers/notes.js
+++ api/src/controllers/notes.js
@@ -4,6 +4,40 @@ const models = require('../models');
 
 const router = express.Router();
 
+function postFilter(obj) {
+  return _.pick(obj, ['title','content','notebookId']);
+}
 /* *** TODO: Fill in the API endpoints for notes *** */
+router.get('/', (req, res) => {
+  models.Note.findAll({ order: [['createdAt', 'DESC']] })
+    .then(note => res.json(note))
+    .catch(err => res.status(500).json({ error: err.message }));
+});
+
+router.post('/',(req,res) => {
+  models.Note.create(postFilter(req.body))
+  .then(note => res.json(note))
+  .catch(err => res.status(422).json({ error: err.message }));
+});
+
+router.get('/:noteId',(req,res) => {
+  models.Note.findById(req.params.noteId)
+  .then(note => res.json(note))
+  .catch(err => res.status(500).json({ error: err.message }));
+});
+
+router.delete('/:noteId',(req,res) => {
+  models.Note.findById(req.params.noteId)
+    .then(note => note.destroy())
+    .then(() => res.json({}))
+    .catch(err => res.status(500).json({ error: err.message }));
+});
+
+router.put('/:noteId',(req,res) => {
+  models.Note.findById(req.params.noteId)
+  .then(note=> note.update(postFilter(req.body)))
+  .then(note => res.json(note))
+  .catch(err => res.status(422).json({ error: err.message }));
+});
 
 module.exports = router;
